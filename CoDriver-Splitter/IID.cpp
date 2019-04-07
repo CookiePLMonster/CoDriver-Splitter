db292bb14a01f2c3c5178f16eb64e7af89c62e58
Silent <zdanio95@gmail.com>
2019-04-07 11:49:30 +0200

    Moved GUID to a separate file

---
+++ CoDriver-Splitter/IID.cpp
@@ -0,0 +1,8 @@
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#define WINVER 0x0601
+#define _WIN32_WINNT 0x0601
+#include <Windows.h>
+
+#include <initguid.h>
+#include "IID.h"
--- CoDriver-Splitter/MOXAudio2.cpp
+++ CoDriver-Splitter/MOXAudio2.cpp
@@ -8,19 +8,10 @@
 #include <xaudio2.h>
 #include <string>
 
-#include <vector>
-#include <algorithm>
-
 #include "MOXAudio2_Common.h"
 #include "MOXAudio2_Hooks.h"
 
-#include <initguid.h>
-
-// {6CE3D7FC-ED78-4256-BCAF-1088C153C9EA}
-DEFINE_GUID(IID_MOXAudio2, 
-	0x6ce3d7fc, 0xed78, 0x4256, 0xbc, 0xaf, 0x10, 0x88, 0xc1, 0x53, 0xc9, 0xea);
-
-class __declspec(uuid("6CE3D7FC-ED78-4256-BCAF-1088C153C9EA")) MOXAudio2;
+#include "IID.h"
 
 class MOXAudio2 final : public IXAudio2
 {
